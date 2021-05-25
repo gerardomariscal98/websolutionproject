992dfaf329de4a8f8c2a602149e84735f420fac7
gerardomariscal98 <84681543+gerardomariscal98@users.noreply.github.com>
2021-05-25 22:31:53 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,15 +1,13 @@
 var video = document.getElementById("video1")
 
-var codigo = document.getElementById("boxRegis").value;
-
-function cambiar() {
-  codigo = "elihu"
-}
-
-if (codigo == "1010") {
-  cambiar();
-}
+function enviar() {
+  var codigo = document.getElementById("boxRegis").value;
   
+  if (codigo == "1010") {
+    codigo = "elihu";
+  }
+
+
   
 
   
