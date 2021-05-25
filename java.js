9f13305994c86ced1939b6cd5515d98e638fe53d
gerardomariscal98 <84681543+gerardomariscal98@users.noreply.github.com>
2021-05-26 00:10:13 +0100

    Update java.js

--- java.js
+++ java.js
@@ -2,8 +2,20 @@ var video = document.getElementById("video1")
 var codigo1 = document.getElementById("boxRegis").value;
 
 function cambiar() {
-  if (document.getElementById("boxRegis").value == "1010") {
+  else if (document.getElementById("boxRegis").value == "1010") {
     document.getElementById("boxRegis").value = "elihu";
+  }
+
+  else if (document.getElementById("boxRegis").value == "2020") {
+    document.getElementById("boxRegis").value = "ximena";
+  }
+
+  else if (document.getElementById("boxRegis").value == "3030") {
+    document.getElementById("boxRegis").value = "aeleen";
+  }
+
+  else if (document.getElementById("boxRegis").value == "4040") {
+    document.getElementById("boxRegis").value = "ematoma";
   }
 }
 
