1d9641018d555b4ab476b63bc9eb01e22712955a
gerardomariscal98 <84681543+gerardomariscal98@users.noreply.github.com>
2021-05-25 20:46:08 +0100

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,24 +1,13 @@
 <?php
-$name = $_POST['nombre'];
-$mail = $_POST['email'];
-$phone = $_POST['tel'];
-$message = $_POST['codigo'];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+$destino = "pelisenp4@gmail.com";
+$nombre = $_POST["nombre"];
+$correo = $_POST["email"];
+$telefono = $_POST["tel"];
+$mensaje = $_POST["codigo"];
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nTeléfono: " . $telefono . "\nCodigo: " . $mensaje;
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['codigo'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
+mail($destino, "Contacto", $contenido);
+header("Location:index.html")
 
-$para = 'e.gerardomariscal@gmail.com';
-$asunto = 'Mensaje de PRECOMPC';
-
-mail($para, $asunto, utf8_decode($message), $header);
-
-header("Location:index.html");
 ?>
