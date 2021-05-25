0f349203ed7a9578d029a29453be3c0e7dc04b4d
gerardomariscal98 <84681543+gerardomariscal98@users.noreply.github.com>
2021-05-25 23:31:06 +0100

    Create java.js

--- java.js
+++ java.js
@@ -3,14 +3,10 @@ var video = document.getElementById("video1")
 
 function cambiar() {
   var codigo = document.getElementById("boxRegis").value;
-
   if (codigo == "1010") {
     codigo = "elihu";
   }
-
-  alert(codigo);
-}
-
+};
 
 
 
